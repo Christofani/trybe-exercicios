c753974f7b5c6436fe43a5710adcf8dc140ee301
Rodrigo Christofani <christofanir@gmail.com>
2023-05-02 20:16:50 -0300

    IF/ELSE 2

---
+++ fundamentos/secoes/secao-3-introducao-js/dia-1-primeiros-passos/exercicio-1/exec3.js
@@ -0,0 +1,11 @@
+let numero1 = 26;
+let numero2 = 34;
+let numero3 = 47;
+
+if (numero1 > numero2 && numero1 > numero3) {
+    console.log("Número 1 é o maior!")
+} else if (numero2 > numero1 && numero2 > numero3) {
+    console.log("Número 2 é o maior!")
+}else if (numero3 > numero1 && numero3 > numero2) {
+    console.log("Número 3 é o maior!")
+}
