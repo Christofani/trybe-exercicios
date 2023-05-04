b127d46502992036a658bf5cb8b017cb8458d829
Rodrigo Christofani <christofanir@gmail.com>
2023-05-04 13:27:40 -0300

    Exercício 1 completo

--- fundamentos/secoes/secao-3-introducao-js/dia-2-array-loop-for/exercicios/exec1.js
+++ fundamentos/secoes/secao-3-introducao-js/dia-2-array-loop-for/exercicios/exec1.js
@@ -62,10 +62,52 @@ for (let index = 1; index <= 25; index += 1) {
 }
 console.log(meuArray)
 
-for (let index = 0; index < meuArray.length; index += 1) {
-     let numerosdiv = meuArray[index] / 2
-     console.log(numerosdiv)
+for (let index1= 0; index1 < meuArray.length; index1 += 1) {
+    Ndivididos = meuArray[index1]
+    
+    console.log(Ndivididos/2)
 }
+
+//Fatorial
+
+let fatorial = 1;
+
+for (let index = 10; index > 0;index -= 1) {
+    fatorial *= index;
+}
+console.log(fatorial)
+
+let palavra = "Tryber"
+let palavraInvertida = "";
+
+for (let index = 0; index < palavra.length; index +=1) {
+    palavraInvertida += palavra[palavra.length - 1 - index]
+}
+console.log(palavraInvertida)
+
+//Array de STRINGS
+
+let array = ['java','javascript','python','html','css']
+
+let  menorPav = array[0];
+let  maiorPav = array[0];
+
+for (let index =1; index < array.length; index += 1) {
+    if (array[index].length > maiorPav.length) {
+        maiorPav = array[index];
+    }
+}
+
+for (let index = 1; index < array.length; index += 1) {
+    if (array[index].length < menorPav.length) {
+        menorPav = array[index];
+    }
+}
+console.log(maiorPav);
+console.log(menorPav);
+
+
+
 
 
 
