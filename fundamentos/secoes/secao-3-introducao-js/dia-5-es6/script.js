59328d55553c9c1be0c4d7f1b96122ea8af90c3d
Rodrigo Christofani <christofanir@gmail.com>
2023-05-08 16:15:23 -0300

    Sobre  var

--- fundamentos/secoes/secao-3-introducao-js/dia-5-es6/script.js
+++ fundamentos/secoes/secao-3-introducao-js/dia-5-es6/script.js
@@ -3,3 +3,4 @@ var userName = 'Lucas';
 console.log(userName); // Resultado: Lucas
 //Outro grande problema ao utilizar o var para atribuir valor a variáveis é que você consegue sobrescrevê-las sem gerar erros.
 
+//Em aplicações maiores, ao usar o var, você pode redeclarar variáveis acidentalmente e não perceber o que aconteceu. Por isso, encontrar e corrigir bugs pode se tornar uma verdadeira missão impossível. ⚠️ Por esses motivos, declarar variáveis com o var não é uma boa prática.
