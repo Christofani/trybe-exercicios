90c753fb88605eec47167296a0b8a23c1af7acab
Rodrigo Christofani <christofanir@gmail.com>
2023-05-08 16:05:34 -0300

    ES6 variáveis

---
+++ fundamentos/secoes/secao-3-introducao-js/dia-5-es6/script.js
@@ -0,0 +1,5 @@
+var userName = 'Isabella';
+var userName = 'Lucas';
+console.log(userName); // Resultado: Lucas
+//Outro grande problema ao utilizar o var para atribuir valor a variáveis é que você consegue sobrescrevê-las sem gerar erros.
+
