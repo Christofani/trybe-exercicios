134252e42d6fb052a4acc0eeba88ab8fa3c16c8b
Rodrigo Christofani <christofanir@gmail.com>
2023-05-12 15:55:53 -0300

    Buscando elementos

---
+++ fundamentos/secoes/secao-4-js-dom/dia-2-elementos/exercicios-course/script2.js
@@ -0,0 +1,5 @@
+// arquivo script.js
+
+console.log(document.getElementById('start').nextSibling) // nó ,me mostra oque está entre os elementos ,proxima 'linha'
+
+console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
