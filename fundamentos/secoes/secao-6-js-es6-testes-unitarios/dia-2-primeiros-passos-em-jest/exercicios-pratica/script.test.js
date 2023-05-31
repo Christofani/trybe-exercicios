81f5e92f094a5f122926be9807abb12fff51e94f
Rodrigo Christofani <christofanir@gmail.com>
2023-05-30 21:31:56 -0300

    Exercício 3

--- fundamentos/secoes/secao-6-js-es6-testes-unitarios/dia-2-primeiros-passos-em-jest/exercicios-pratica/script.test.js
+++ fundamentos/secoes/secao-6-js-es6-testes-unitarios/dia-2-primeiros-passos-em-jest/exercicios-pratica/script.test.js
@@ -1,4 +1,4 @@
-const {removeItem, myFizzBuzz} = require('./script');
+const {removeItem, myFizzBuzz, encode , decode} = require('./script');
 
 describe('testa a função removeItem', () => {
     test('Deve retornar o array removendo o item correto.', () => {
@@ -15,5 +15,26 @@ describe('testa a função myFizzBuzz', () => {
         expect(myFizzBuzz(5)).toBe('buzz');
         expect(myFizzBuzz(2)).toBe(2);
         expect(myFizzBuzz('2')).toBeFalsy();
+    });
+});
+
+describe('teste as funções encode e decode', () => {
+    test('Verifique se encode e decode são funções', () => {
+        expect(typeof encode).toBe('function')
+        expect(typeof decode).toBe('function')
+    });
+    test('Em encode teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4, 5, respectivamente.', () => {
+        expect(encode('ana')).toEqual('1n1');
+        expect(encode('troll')).toEqual('tr4ll');
+        expect(encode('ellen')).toEqual('2ll2n');
+        expect(encode('ivinis')).toEqual('3v3n3s');
+        expect(encode('hurus')).toEqual('h5r5s');
+    });
+    test('Em decode teste se os números 1, 2, 3, 4, 5 são convertidos nas vogais a, e, i, o, u, respectivamente.', () => {
+        expect(decode('1n1')).toEqual('ana');
+        expect(decode('tr4ll')).toEqual('troll');
+        expect(decode('2ll2n')).toEqual('ellen');
+        expect(decode('3v3n3s')).toEqual('ivinis');
+        expect(decode('h5r5s')).toEqual('hurus');
     });
 });
