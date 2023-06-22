959615390ae05f7cfec79c70a7c4bdd411616b71
Rodrigo Christofani <christofanir@gmail.com>
2023-06-21 21:27:58 -0300

    Aplicação pronta

--- front-end/secoes/secao-1-introducao-a-frontend-JS-assincrono/dia-1-ambiente-de-desenvolvimento/exercicio-validador-de-campos/src/main.js
+++ front-end/secoes/secao-1-introducao-a-frontend-JS-assincrono/dia-1-ambiente-de-desenvolvimento/exercicio-validador-de-campos/src/main.js
@@ -1,19 +1,21 @@
-import  validator  from "validator";
+import validator from 'validator';
+import './style.css';
 
 const buttonValidator = document.querySelector('#validator');
 const selectValidator = document.querySelector('#valid');
 const valueInserid = document.querySelector('#value');
 const explicationParagraf = document.querySelector('#explication');
+const UUID_VERSION = 4;
 
 buttonValidator.addEventListener('click', (event) => {
- event.preventDefault();
- const campos = {
-  cpf: validator.isTaxID(valueInserid.value, 'pt-BR'),
-  email: validator.isEmail(valueInserid.value),
-  uuid: validator.isUUID(valueInserid.value, 4),
-  url: validator.isURL(valueInserid.value),
-  hexColor: validator.isHexColor(valueInserid.value),
- };
+  event.preventDefault();
+  const campos = {
+    cpf: validator.isTaxID(valueInserid.value, 'pt-BR'),
+    email: validator.isEmail(valueInserid.value),
+    uuid: validator.isUUID(valueInserid.value, UUID_VERSION),
+    url: validator.isURL(valueInserid.value),
+    hexColor: validator.isHexColor(valueInserid.value),
+  };
 
- explicationParagraf.innerHTML = `A validação retornou ${campos[selectValidator.value]}`
+  explicationParagraf.innerHTML = `A validação retornou ${campos[selectValidator.value]}`;
 });
