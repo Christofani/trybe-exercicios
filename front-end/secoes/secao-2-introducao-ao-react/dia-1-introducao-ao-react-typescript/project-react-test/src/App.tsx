bc43bd38c3f07aa7372e68ae66ecc33e40462df8
Rodrigo Christofani <christofanir@gmail.com>
2023-07-03 13:58:38 -0300

    criand projeto teste react TypeScript, componentes..

--- front-end/secoes/secao-2-introducao-ao-react/dia-1-introducao-ao-react-typescript/project-react-test/src/App.tsx
+++ front-end/secoes/secao-2-introducao-ao-react/dia-1-introducao-ao-react-typescript/project-react-test/src/App.tsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div>
       <Header />
-      <p>Hello World</p>
     </div>
   );
 }
--- front-end/secoes/secao-2-introducao-ao-react/dia-1-introducao-ao-react-typescript/project-react-test/src/components/header.tsx
+++ front-end/secoes/secao-2-introducao-ao-react/dia-1-introducao-ao-react-typescript/project-react-test/src/components/header.tsx
@@ -1,5 +1,30 @@
 function Header() {
-  return <h1>Olá</h1>;
+  const firstName = 'Rodrigo';
+  const lastName = 'Christofani';
+
+  function luckyNumber() {
+    return Math.floor(Math.random() * 60 + 1);
+  }
+
+  return (
+    <div>
+      <h1 className="greeting">
+        Olá
+        {' '}
+        {`${firstName} ${lastName}`}
+        <img src="https://i.imgur.com/rqvLd3q.png" alt="" />
+      </h1>
+      <h2 className="subtitle">Seus números da sorte da Mega-Sena são:</h2>
+      <ul>
+        <li>{luckyNumber()}</li>
+        <li>{luckyNumber()}</li>
+        <li>{luckyNumber()}</li>
+        <li>{luckyNumber()}</li>
+        <li>{luckyNumber()}</li>
+        <li>{luckyNumber()}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
