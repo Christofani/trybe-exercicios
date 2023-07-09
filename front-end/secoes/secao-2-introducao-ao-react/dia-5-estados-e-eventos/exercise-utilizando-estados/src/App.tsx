80d9b0758c9f36c1aafd86164e421a21af54a99f
Rodrigo Christofani <christofanir@gmail.com>
2023-07-09 20:14:26 -0300

    Atualizando Listas e Objetos

--- front-end/secoes/secao-2-introducao-ao-react/dia-5-estados-e-eventos/exercise-utilizando-estados/src/App.tsx
+++ front-end/secoes/secao-2-introducao-ao-react/dia-5-estados-e-eventos/exercise-utilizando-estados/src/App.tsx
@@ -12,9 +12,11 @@ function App() {
     'Testes automatizados',
   ];
   const [index, setIndex] = useState(0);
+  const [toolList, setToolList] = useState(toolKit)
+  const [inputValue, setInputValue] = useState('');
 
   function handleNextClick() {
-    if (index + 1 < toolKit.length) {
+    if (index + 1 < toolList.length) {
       setIndex(index + 1);
     } else {
       setIndex(0); // Volta para o primeiro elemento
@@ -25,16 +27,35 @@ function App() {
     if (index - 1 >= 0) {
       setIndex(index - 1);
     } else {
-      setIndex(toolKit.length - 1);
+      setIndex(toolList.length - 1);
+    }
+  }
+
+  function handleAddClick() {
+    if (inputValue) {
+      setToolList([...toolList, inputValue]);
     }
   }
 
   return (
     <>
     <h1>Caixa de ferramentas de uma Pessoa Desenvolvedora</h1>
-    <h2>{ toolKit[index] }</h2>
+    <h2>{ toolList[index] }</h2>
     <button onClick={handlePreviousClick}>Anterior</button>
     <button onClick={handleNextClick}>Próximo</button>
+    <section>
+      <h3>Adicione novas ferramentas:</h3>
+      <input onChange={({ target }) => setInputValue(target.value)}/>
+      <button onClick={handleAddClick}>Adicionar</button>
+    </section>
+    <section>
+      <h3>Lista de itens do carrosel:</h3>
+      <ul>
+        {toolList.map((tool) => (
+          <li>{tool}</li>
+        ))}
+      </ul>
+    </section>
     </>
   )
 }
