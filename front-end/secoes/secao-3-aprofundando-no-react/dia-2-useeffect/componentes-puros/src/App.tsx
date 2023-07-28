62cece9af1dab582efaf0a86260d2705816c80eb
Rodrigo Christofani <christofanir@gmail.com>
2023-07-28 11:51:58 -0300

    Initial Commit

---
+++ front-end/secoes/secao-3-aprofundando-no-react/dia-2-useeffect/componentes-puros/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Greeting from './components/Greeting'
+
+function App() {
+  return (
+    <div>
+      <Greeting name="Daniel" />
+      <Greeting name="Rafael" />
+    </div>
+  )
+}
+
+export default App
