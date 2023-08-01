d6f44537adf74db47de1056bb9b7733cc4936ad6
Rodrigo Christofani <christofanir@gmail.com>
2023-08-01 19:26:04 -0300

    exercicio-componentes-puros

--- front-end/secoes/secao-3-aprofundando-no-react/dia-2-useeffect/componentes-puros/src/App.tsx
+++ front-end/secoes/secao-3-aprofundando-no-react/dia-2-useeffect/componentes-puros/src/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import './App.css'
 import Greeting from './components/Greeting'
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
       <Greeting name="Daniel" />
       <Greeting name="Rafael" />
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
+      <h3>{counter}</h3>
     </div>
   )
 }
