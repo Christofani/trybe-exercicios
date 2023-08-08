95c256d1d529f4081bcd2892a9af59337ca3b7f3
Rodrigo Christofani <christofanir@gmail.com>
2023-08-08 11:41:40 -0300

    Exercício finalizado

--- front-end/secoes/secao-3-aprofundando-no-react/dia-2-useeffect/international-space-station/src/App.tsx
+++ front-end/secoes/secao-3-aprofundando-no-react/dia-2-useeffect/international-space-station/src/App.tsx
@@ -10,16 +10,33 @@ type Coordinates = {
 
 function App() {
   const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    async function featchData() {
+    async function fetchData() {
       const data = await fetchCoordinates();
       setCoordinates({ latitude: data.latitude, longitude: data.longitude});
+      setLoading(true);
     }
-    featchData();
+    fetchData();
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 3000);
+
+    return () => { // Essa é a função cleanup
+      clearInterval(intervalId);
+    };
+
+
   }, []);
 
+  if (!loading) {
+    return <h1>Loading...</h1>
+  }
+
   return (
+    
     <div>
       <h1>International Space Station Location Tracker</h1>
       {coordinates && (
