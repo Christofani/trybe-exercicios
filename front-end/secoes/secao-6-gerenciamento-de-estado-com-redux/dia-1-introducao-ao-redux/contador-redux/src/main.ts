04f9b454eb384a7fff3cd4a9c4c22d6cc11906bb
Rodrigo Christofani <christofanir@gmail.com>
2023-08-24 18:59:13 -0300

    Resolução exercício Redux na Prática

--- front-end/secoes/secao-6-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/contador-redux/src/main.ts
+++ front-end/secoes/secao-6-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/contador-redux/src/main.ts
@@ -1,24 +1,39 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { legacy_createStore as createStore} from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+type ActionType = {
+  type: string;
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const INITIAL_STATE = {
+  count: 0,
+};
+
+const reducer = (state = INITIAL_STATE, action: ActionType) => {
+  switch (action.type) {
+    case "INCREMENT_COUNTER":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer, composeWithDevTools());
+
+const action = {
+  type: "INCREMENT_COUNTER",
+}
+
+const button = document.querySelector("button") as HTMLButtonElement;
+button.addEventListener("click", () => {
+  store.dispatch(action);
+  
+});
+
+store.subscribe(() => {
+  const state = store.getState();
+  const countElement = document.querySelector("h2") as HTMLHeadingElement;
+  countElement.innerText = state.count.toString();
+});
