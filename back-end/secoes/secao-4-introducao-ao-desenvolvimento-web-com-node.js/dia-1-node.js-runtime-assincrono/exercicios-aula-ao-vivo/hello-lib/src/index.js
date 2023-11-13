ff7bd60880f1eca8a0c5824593077a56a12c878a
Rodrigo Christofani <christofanir@gmail.com>
2023-11-13 18:47:38 -0300

    Exercícios agora é prática Part 1

--- back-end/secoes/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-1-node.js-runtime-assincrono/exercicios-aula-ao-vivo/hello-lib/src/index.js
+++ back-end/secoes/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-1-node.js-runtime-assincrono/exercicios-aula-ao-vivo/hello-lib/src/index.js
@@ -1,5 +1,8 @@
 const readline = require('readline-sync');
+const os = require('os');
+const fs = require("fs");
 
 const name = readline.question('What is your name? ');
 
 console.log(`Hello, ${name}!`);
+console.log(`Você está usando um computador com o seguinte processador`, os.cpus());
