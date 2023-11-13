342e4c7335e004d17aea3a2b3442ef3d7b642b11
Rodrigo Christofani <christofanir@gmail.com>
2023-11-13 19:47:56 -0300

    Configurando Eslint

--- back-end/secoes/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-1-node.js-runtime-assincrono/exercicios-aula-ao-vivo/hello-lib/src/index.js
+++ back-end/secoes/secao-4-introducao-ao-desenvolvimento-web-com-node.js/dia-1-node.js-runtime-assincrono/exercicios-aula-ao-vivo/hello-lib/src/index.js
@@ -1,8 +1,15 @@
 const readline = require('readline-sync');
-const os = require('os');
-const fs = require("fs");
+// Const os = require('os');
+const fs = require('fs').promises;
+const path = require('path');
 
 const name = readline.question('What is your name? ');
 
 console.log(`Hello, ${name}!`);
-console.log(`Você está usando um computador com o seguinte processador`, os.cpus());
+// Console.log(`Você está usando um computador com o seguinte processador`, os.cpus());
+const filePath = path.resolve(__dirname, 'test.json');
+console.log(`Arquivo : ${filePath}`);
+
+fs.writeFile(filePath, 'Conteúdo')
+	.then(() => console.log('Escrita deu certo'))
+	.catch(() => console.log('Escrita deu errado'));
