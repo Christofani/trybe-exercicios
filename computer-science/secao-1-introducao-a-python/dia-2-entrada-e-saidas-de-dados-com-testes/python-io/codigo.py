55150d8e90b47d10727c81494251d630b0f5a4b8
Rodrigo Christofani <christofanir@gmail.com>
2024-06-07 17:43:06 -0300

    Testes automatizados.

---
+++ computer-science/secao-1-introducao-a-python/dia-2-entrada-e-saidas-de-dados-com-testes/python-io/codigo.py
@@ -0,0 +1,11 @@
+def is_odd(number):
+    "Retorna true se um número é impar , senão false."
+    return number % 2 != 0
+
+
+# ...
+
+
+def divide(a_number, other_number):
+    "Retorna a divisão de a_number por other_number"
+    return a_number / other_number
--- computer-science/secao-1-introducao-a-python/dia-2-entrada-e-saidas-de-dados-com-testes/python-io/reading_json.py
+++ computer-science/secao-1-introducao-a-python/dia-2-entrada-e-saidas-de-dados-com-testes/python-io/reading_json.py
@@ -5,3 +5,5 @@ with open("pokemons.json") as file:
     pokemons = json.load(file)["results"]
 
 print(pokemons[0])  # imprime o primeiro pokemon da lista
+
+file.close()
